b7f6c9b4d9fccecb24b80e75663b3c83b0ceec79
Eduardo Santos Gomes ferreira <106843885+EspadachiNNegr0@users.noreply.github.com>
2024-05-24 00:56:07 -0300

    Update index.js

--- js/index.js
+++ js/index.js
@@ -31,3 +31,18 @@ function addTodo() {
         alert("Por favor, insira uma URL válida.");
     }
 }
+
+const search = document.querySelector('.search');
+const btn = document.querySelector('.btn');
+
+btn.addEventListener('click', () => {
+    const url = 'https://www.google.com.br/search?q=' + search.value;
+    window.open(url,'_self');
+})
+search.addEventListener('keypress', function(e) {
+    if(e.key === "Enter"){
+        const url = 'https://www.google.com.br/search?q=' + search.value;
+        search.click();
+        window.open(url,'_self');
+    }
+})
